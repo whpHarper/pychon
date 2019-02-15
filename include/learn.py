494fb9ec153b6f3b032c29ea5519295f37c42169
wanghaipeng <whp15369657805@163.com>
2019-02-15 18:18:07 +0800

    上传学习代码

--- include/learn.py
+++ include/learn.py
@@ -2,7 +2,6 @@ from elasticsearch5 import Elasticsearch
 
 es = Elasticsearch([{"host": "59.110.18.77", "port": 9200}])
 res = es.search(index="sadb-2018.11.12", doc_type="hiversl")
-
 print(res['hits']['hits'])
 for hit in res['hits']['hits']:
     print(hit["_source"]["reqid"],hit["_source"]["ts"])
